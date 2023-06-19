686bb14830de8075b6e6dcbe0aaf3209dcd4cdfb
ekawibawaputu <ekawibawaputu@gmail.com>
2023-06-20 02:39:58 +0800

    feat : slicing detail movie

--- routes/web.php
+++ routes/web.php
@@ -40,5 +40,9 @@ Route::prefix('prototype')->name('prototype.')->group(function() {
         return Inertia::render('Prototype/SubscriptionPlan');
     })->name('subscriptionPlan');
 
+    route::get('/movie/{slug}', function(){
+        return Inertia::render('Prototype/Movie/Show');
+    })->name('movie.show');
+
 });
 require __DIR__.'/auth.php';
